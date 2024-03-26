816cd3a31e818bc7e5a4c54b41acda324ca2a825
333suki <159127649+333suki@users.noreply.github.com>
2024-03-26 09:24:15 +0100

    Create A1W1A1.py

---
+++ Arch1/Week1/A1W1A1.py
@@ -0,0 +1,5 @@
+years = input("Choose any amount of years: ")
+months = int(years) * 12
+days = int(years) * 365
+
+print(str(years) + " years consists of " + str(months) + " months and " + str(days) + " days.")
