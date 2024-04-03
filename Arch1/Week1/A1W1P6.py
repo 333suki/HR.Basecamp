583e0a4f829538e0ceb2bd06791a9d7f5d6a273d
333suki <159127649+333suki@users.noreply.github.com>
2024-04-03 16:40:53 +0200

    Create A1W1P6.py

---
+++ Arch1/Week1/A1W1P6.py
@@ -0,0 +1,5 @@
+days = input("Choose any amount of days: ")
+hours = int(days) * 24
+minutes = int(days) * 1440
+seconds = int(days) * 86400
+print(str(days) + " days consist of " + str(hours) + " hours and " + str(minutes) + " minutes and " + str(seconds) + " seconds.")
