c3da86864b4f4f15411b83512e37cc9b876430c3
Suki <159127649+333suki@users.noreply.github.com>
2024-06-14 16:30:09 +0200

    Finished vessel.py

--- Master_Assignment/vessel.py
+++ Master_Assignment/vessel.py
@@ -2,7 +2,7 @@ import sqlite3
 
 class Vessel:
 
-    def __init__(self, imo: int, mmsi: int, name: str, country: str, type: str, build: int, gross: int, netto: int, length: int, beam: int) -> None:
+    def __init__(self, imo: int, mmsi: int | None, name: str, country: str, type: str, build: int, gross: int, netto: int, length: int, beam: int) -> None:
         self.imo = imo
         self.mmsi = mmsi
         self.name = name
@@ -15,7 +15,7 @@ class Vessel:
         self.beam = beam
 
 
-    def get_shipments(self): # Returns a tuple of Shipments for this vessel
+    def get_shipments(self):  # Returns a tuple of Shipments for this vessel
         from shipment import Shipment
 
         con = sqlite3.connect("shipments.db")
@@ -28,16 +28,16 @@ class Vessel:
         """
 
         cur.execute(query, [self.imo])
-        results = cur.fetchall()
-        for result in results:
-            shipments.append(Shipment(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], 
+        query_results = cur.fetchall()
+        for result in query_results:
+            shipments.append(Shipment(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],
                                       result[8]))
 
         con.close()
         return tuple(shipments)
 
 
-    def get_fuel_consumption(self, distance): # Returns a number based on calculations
+    def get_fuel_consumption(self, distance):  # Returns a number based on calculations
         ship_types = {
             "Aggregates Carrier": 0.4,
             "Bulk Carrier": 0.35,
@@ -70,5 +70,5 @@ class Vessel:
         return "{}({})".format(type(self).__name__, ", ".join([f"{key}={value!s}" for key, value in self.__dict__.items()]))
 
 vessel = Vessel(1034034, None, "GREAT WALL 17", "Tanzania", "Deck Cargo Ship", 2023, 1978, 2373, 84, 19)
-print(vessel.get_shipments)
-print(vessel.get_fuel_consumption(1500))
+# print(vessel.get_shipments)
+# print(vessel.get_fuel_consumption(1000))
