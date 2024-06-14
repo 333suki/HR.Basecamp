fd9660ddc201c7451c936b1500f259a5e65638de
Suki <159127649+333suki@users.noreply.github.com>
2024-06-14 16:29:10 +0200

    Finished shipmentreporter.py

--- Master_Assignment/shipmentreporter.py
+++ Master_Assignment/shipmentreporter.py
@@ -21,14 +21,14 @@ class Reporter:
     # How many vessels are there? -> int
     def total_amount_of_vessels(self) -> int:
         return self.con.execute("""SELECT COUNT(*) FROM vessels""").fetchone()[0]
-    
+
     # What is the longest shipment distance? -> Shipment
     def longest_shipment(self) -> Shipment:
         query_results = self.con.execute("""SELECT * FROM shipments ORDER BY distance_naut DESC""").fetchone()
         if not query_results:
             return None
         return Shipment(*query_results)
-    
+
     # What is the longest and shortest vessel? -> tuple[Vessel, Vessel]
     def longest_and_shortest_vessels(self) -> tuple[Vessel, Vessel]:
         query_results = self.con.execute("""SELECT * FROM vessels ORDER BY length, imo DESC""").fetchall()
@@ -47,7 +47,7 @@ class Reporter:
     # Which vessels have the most shipments -> tuple[Vessel, ...]
     def vessels_with_the_most_shipments(self) -> tuple[Vessel, ...]:
         query_results = self.con.execute("""SELECT vessel FROM shipments""").fetchall()
-        vessel_count = {} # Dict to store the IDs as keys and the amount of times it occured as values
+        vessel_count = {}  # Dict to store the IDs as keys and the amount of times it occured as values
         highest_shipments = []
         vessel_obj = []
 
@@ -56,20 +56,20 @@ class Reporter:
                 vessel_count[vessel[0]] += 1
             else:
                 vessel_count[vessel[0]] = 1
-        
+
         highest_value = max(vessel_count.values())
         for vessel_id, shipment_count in vessel_count.items():
             if highest_value == shipment_count:
                 highest_shipments.append(vessel_id)
-                
-        for item in highest_shipments: # Get the vessel details and create a Vessel object
+
+        for item in highest_shipments:  # Get the vessel details and create a Vessel object
             query = """SELECT * FROM vessels
                         WHERE imo = ? LIMIT 1"""
             query_results = self.con.execute(query, [item]).fetchone()
             vessel_obj.append(Vessel(*query_results))
 
         return tuple(vessel_obj)
-    
+
     # Which ports have the most shipments -> tuple[Port, ...]
     def ports_with_most_shipments(self) -> tuple[Port, ...]:
         query_results = self.con.execute("""SELECT origin, destination FROM shipments""").fetchall()
@@ -90,11 +90,11 @@ class Reporter:
 
             highest_value = max(port_count.values())
 
-        for port_id, shipment_count in port_count.items(): # Find the ports with the highest shipment count
+        for port_id, shipment_count in port_count.items():  # Find the ports with the highest shipment count
             if highest_value == shipment_count:
                 highest_shipments.append(port_id)
-            
-        for item in highest_shipments: # Get the port details and create a Port object
+
+        for item in highest_shipments:  # Get the port details and create a Port object
             query = """SELECT * FROM ports WHERE id = ? LIMIT 1"""
             query_results = self.con.execute(query, [item]).fetchone()
             port_obj.append(Port(*query_results))
@@ -115,10 +115,10 @@ class Reporter:
             else:
                 dates = set(result[0] for result in self.con.execute("""SELECT date FROM shipments ORDER BY date""").fetchall())
                 for _date in sorted(dates):
-                    vessel_imos = [result[0] for result in self.con.execute("""SELECT vessel FROM shipments 
+                    vessel_imos = [result[0] for result in self.con.execute("""SELECT vessel FROM shipments
                                                                             WHERE date = ?""", [_date]).fetchall()]
                     for vessel_imo in vessel_imos:
-                        db_vessel_type = self.con.execute("""SELECT type FROM vessels 
+                        db_vessel_type = self.con.execute("""SELECT type FROM vessels
                                                         WHERE imo = ? LIMIT 1""", [vessel_imo]).fetchone()
                         if db_vessel_type[0] == vessel_type:
                             first_date = _date
@@ -126,22 +126,22 @@ class Reporter:
                     if first_date:
                         break
 
-            if vessel_type is None: # Get port IDs for the first shipment date
-                query_results = self.con.execute("""SELECT origin FROM shipments 
+            if vessel_type is None:  # Get port IDs for the first shipment date
+                query_results = self.con.execute("""SELECT origin FROM shipments
                                                 WHERE date = ? ORDER BY origin""", [first_date]).fetchall()
                 for result in query_results:
                     port_ids.append(result[0])
             else:
-                query_results = self.con.execute("""SELECT origin, vessel FROM shipments 
+                query_results = self.con.execute("""SELECT origin, vessel FROM shipments
                                                 WHERE date = ? ORDER BY origin""", [first_date]).fetchall()
                 for origin, vessel in query_results:
-                    db_vessel_type = self.con.execute("""SELECT type FROM vessels 
+                    db_vessel_type = self.con.execute("""SELECT type FROM vessels
                                                     WHERE imo = ? LIMIT 1""", [vessel]).fetchone()
                     if db_vessel_type[0] == vessel_type:
                         port_ids.append(origin)
 
-            for port_id in port_ids: # Fetch port details and create Port objects
-                query_result = self.con.execute("""SELECT * FROM ports 
+            for port_id in port_ids:  # Get port details and create Port objects
+                query_result = self.con.execute("""SELECT * FROM ports
                                                 WHERE id = ? LIMIT 1""", [port_id]).fetchone()
                 ports.append(Port(*query_result))
 
@@ -157,14 +157,14 @@ class Reporter:
         latest_date = None
 
         try:
-            if vessel_type is None: # Get the latest shipment date
+            if vessel_type is None:  # Get the latest shipment date
                 latest_date = self.con.execute("""SELECT date FROM shipments ORDER BY date DESC LIMIT 1""").fetchone()[0]
             else:
                 dates = set(result[0] for result in self.con.execute("""SELECT date FROM shipments ORDER BY date DESC""").fetchall())
                 for _date in sorted(dates, reverse=True):
                     vessel_imos = [result[0] for result in self.con.execute("""SELECT vessel FROM shipments WHERE date = ?""", [_date]).fetchall()]
                     for vessel_imo in vessel_imos:
-                        db_vessel_type = self.con.execute("""SELECT type FROM vessels 
+                        db_vessel_type = self.con.execute("""SELECT type FROM vessels
                                                         WHERE imo = ? LIMIT 1""", [vessel_imo]).fetchone()
                         if db_vessel_type[0] == vessel_type:
                             latest_date = _date
@@ -172,22 +172,22 @@ class Reporter:
                     if latest_date:
                         break
 
-            if vessel_type is None: # Get port IDs for the latest shipment date
-                query_results = self.con.execute("""SELECT origin FROM shipments 
+            if vessel_type is None:  # Get port IDs for the latest shipment date
+                query_results = self.con.execute("""SELECT origin FROM shipments
                                                 WHERE date = ? ORDER BY origin""", [latest_date]).fetchall()
                 for result in query_results:
                     port_ids.append(result[0])
             else:
-                query_results = self.con.execute("""SELECT origin, vessel FROM shipments 
+                query_results = self.con.execute("""SELECT origin, vessel FROM shipments
                                                 WHERE date = ? ORDER BY origin""", [latest_date]).fetchall()
                 for origin, vessel in query_results:
-                    db_vessel_type = self.con.execute("""SELECT type FROM vessels 
+                    db_vessel_type = self.con.execute("""SELECT type FROM vessels
                                                     WHERE imo = ? LIMIT 1""", [vessel]).fetchone()
                     if db_vessel_type[0] == vessel_type:
                         port_ids.append(origin)
 
-            for port_id in port_ids: # Get port details and create Port objects
-                query_result = self.con.execute("""SELECT * FROM ports 
+            for port_id in port_ids:  # Get port details and create Port objects
+                query_result = self.con.execute("""SELECT * FROM ports
                                                 WHERE id = ? LIMIT 1""", [port_id]).fetchone()
                 ports.append(Port(*query_result))
 
@@ -218,7 +218,7 @@ class Reporter:
 
         for vessel_id in vessel_ids:
             query_result = self.con.execute(
-                """SELECT * FROM vessels 
+                """SELECT * FROM vessels
                 WHERE imo = ? LIMIT 1""",[vessel_id]).fetchone()
             vessels.append(Vessel(*query_result))
 
@@ -236,8 +236,8 @@ class Reporter:
                 ])
 
             filename = f"Vessels docking Port {port.id} between {start.strftime('%Y-%m-%d')} and {end.strftime('%Y-%m-%d')}.csv"
-            file = open(filename, "wt", newline = "", encoding = "utf-8")
-            writer = csv.writer(file, delimiter = ",")
+            file = open(filename, "wt", newline="", encoding="utf-8")
+            writer = csv.writer(file, delimiter=",")
             writer.writerows(to_write)
             file.close()
 
@@ -252,7 +252,7 @@ class Reporter:
     #   id, code, name, city, province, country
     def ports_in_country(self, country: str, to_csv: bool = False) -> tuple[Port, ...] | None:
         ports = []
-        query_results = self.con.execute("""SELECT * FROM ports 
+        query_results = self.con.execute("""SELECT * FROM ports
                                          WHERE country = ? ORDER BY id""",[country]).fetchall()
 
         for result in query_results:
@@ -271,8 +271,8 @@ class Reporter:
                 ])
 
             filename = f"Ports in country {country}.csv"
-            file = open(filename, "wt", newline = "", encoding = "utf-8")
-            writer = csv.writer(file, delimiter = ",")
+            file = open(filename, "wt", newline="", encoding="utf-8")
+            writer = csv.writer(file, delimiter=",")
             writer.writerows(to_write)
             file.close()
 
@@ -287,7 +287,7 @@ class Reporter:
     #   imo, mmsi, name, country, type, build, gross, netto, length, beam
     def vessels_from_country(self, country: str, to_csv: bool = False) -> tuple[Vessel, ...] | None:
         vessels = []
-        query_results = self.con.execute("""SELECT * FROM vessels 
+        query_results = self.con.execute("""SELECT * FROM vessels
                                          WHERE country = ?""",[country]).fetchall()
 
         for result in query_results:
@@ -307,8 +307,8 @@ class Reporter:
                 ])
 
             filename = f"Vessels from country {country}.csv"
-            file = open(filename, "wt", newline = "", encoding = "utf-8")
-            writer = csv.writer(file, delimiter = ",")
+            file = open(filename, "wt", newline="", encoding="utf-8")
+            writer = csv.writer(file, delimiter=",")
             writer.writerows(to_write)
             file.close()
 
